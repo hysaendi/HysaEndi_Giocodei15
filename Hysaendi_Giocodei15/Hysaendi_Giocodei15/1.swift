a23b3041378961f3bbb59bf213c3cf18166da24e
Endi <endi.watch@gmail.com>
2019-12-18 12:58:00 +0100

    Inserimento metodo Vittoria

--- Hysaendi_Giocodei15/Hysaendi_Giocodei15/1.swift
+++ Hysaendi_Giocodei15/Hysaendi_Giocodei15/1.swift
@@ -10,8 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var lbl_Mosse: UILabel!
     @IBOutlet var ArrButtons: [UIButton]!
-    
+    var mos = 0
     var mat = [[1, 2, 3, 4],
                [5, 6, 7, 8],
                [9, 10, 11, 12],
@@ -34,6 +35,7 @@ class ViewController: UIViewController {
    
     func scombinaMatrice (mosse : Int)
     {
+        
         var r = 0
         var posizioneZero = trovaLoZero()
         var x = posizioneZero.1
@@ -58,6 +60,7 @@ class ViewController: UIViewController {
                    mat [y][x] = mat [y][x + 1]
                    x += 1
                    mat [y][x] = 0
+                   
                 }
             }
             else if (r == 2)                //sposta giù
@@ -67,6 +70,7 @@ class ViewController: UIViewController {
                     mat [y][x] = mat [y + 1][x]
                     y += 1
                     mat [y][x] = 0
+                    
                 }
             }
             else if (r == 3)            //sposta a sinistra
@@ -76,9 +80,12 @@ class ViewController: UIViewController {
                     mat [y][x] = mat [y][x - 1]
                     x -= 1
                     mat[y][x] = 0
+                   
                 }
             }
         }
+        lbl_Mosse.text = String (0)
+        mos = 0
         
         
     }
@@ -86,6 +93,7 @@ class ViewController: UIViewController {
   
     
     @IBAction func spostaCella(_ sender: Any) {
+        
         var cella : Int!
         cella = ArrButtons.firstIndex(of: sender as! UIButton)
         var y = cella/4
@@ -94,37 +102,43 @@ class ViewController: UIViewController {
         
         if (x > 0)
         {
-            if (mat[y][x-1] == 0)
+            if (mat[y][x-1] == 0)        //sposto a sinistra la casella e scambio il valore
             {
                 mat[y][x-1] = mat [y][x]
                 mat[y][x] = 0
+                mos = mos + 1
             }
         }
         if (y > 0)
         {
-            if (mat[y-1][x] == 0)
+            if (mat[y-1][x] == 0)        //sposto in giu la casella e scambio il valore
             {
                 mat[y-1][x] = mat[y][x]
                 mat[y][x] = 0
+                mos = mos + 1
             }
         }
          if (x < 3)
         {
-            if (mat[y][x+1] == 0)
+            if (mat[y][x+1] == 0)        //sposto a destra la casella e scambio il valore
             {
                 mat[y][x+1] = mat[y][x]
                 mat[y][x] = 0
+                mos = mos + 1
             }
         }
         if (y < 3)
         {
-            if (mat[y+1][x] == 0)
+            if (mat[y+1][x] == 0)       //sposto in su la casella e scambio il valore
             {
                 mat[y+1][x] = mat[y][x]
                 mat[y][x] = 0
+                mos = mos + 1
             }
         }
-        switchBottoni()
+        switchBottoni()    //aggiorno la parte grafica
+        lbl_Mosse.text = String(mos)   //conto le mosse
+        controllaVittoria()
     
     }
     
@@ -155,7 +169,35 @@ class ViewController: UIViewController {
        
       }
     
+    func numInz ()
+    {
+        var mat =     [[1, 2, 3, 4],
+                      [5, 6, 7, 8],
+                      [9, 10, 11, 12],
+                      [13, 14, 15, 0]]
+        
+    }
+    
+    func controllaVittoria ()
+    {
+        var corretto = true
+        var mat2 = mat
+        numInz()
+        for i in 0...15
+        {
+            if (mat2[i] == mat[i]){}
+            else
+            {
+                corretto = false
+            }
+        }
+        if (corretto == true)
+        {
+            let alert = UIAlertController(title: "Hai vinto", message: "Congratulazioni", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in NSLog("The \"OK\" alert occured.")}))
+        }
    
+}
 }
                
         
